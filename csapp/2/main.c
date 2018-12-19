78af78ba14d0d62c1662f720aa864310a5dc69e0
shiqf <jiyuuinchina@hotmail.com>
2018-12-19 22:11:07 +0800

    update

--- csapp/2/main.c
+++ csapp/2/main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+
+/* 定义 INT_MAX、INT_MIN、UINT_MAX */
+/* INT_MIN 的值没有与之对应的正值 */
+#include <limits.h>
+
+/* 定义int类型大小的头文件 */
+/* 提供形如intN_t、uintN_t、INTN_MIN、INTN_MAX、UINTN_MAX */
+#include <stdint.h>
+
+/* 提供宏 PRId32 */
+#include <inttypes.h>
+
+/* 自定义函数声明头文件 */
 #include "02.h"
 
 char s[65];
 int main(int argc, char *argv[])
 {
+
+    /* 补码表示 */
+    /* short x = 12345; */
+    /* short mx = -x; */
+    /* show_bytes((byte_pointer) &x, sizeof(short)); */
+    /* show_bytes((byte_pointer) &mx, sizeof(short)); */
     
-    /* 2.13 */
+    /* /1* 类型大小 *1/ */
+    /* int32_t x = 0x12345678; */
+    /* int64_t y = 0x1234567887654321; */
+    /* printf("x = %" PRId32 ", y = %" PRIu64 "\n", x, y); */
 
     /* 2.12 */
     /* int x = 0x87654321; */
