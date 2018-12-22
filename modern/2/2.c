36bf2317a461cb00feaf06fe79c02e78b03a8140
shiqf <jiyuuinchina@hotmail.com>
2018-12-22 21:53:48 +0800

    add Exercise

---
+++ modern/2/2.c
@@ -0,0 +1,12 @@
+/* 指令 */
+#include <stdio.h>
+
+int main(void)
+{
+    /* 下面三条都是语句 */
+    printf("Parkingson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+
+    return 0;
+}
