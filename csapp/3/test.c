e3e7567791de63d2021374913f13e229ba1a5cac
shiqf <jiyuuinchina@hotmail.com>
2018-12-24 21:34:32 +0800

    update test.c

--- csapp/3/test.c
+++ csapp/3/test.c
@@ -1,5 +1,14 @@
-#define TEST >
-typedef long data_t;
+/* #define TEST >= */
+/* typedef long data_t; */
+
+#define TEST ==
+typedef short data_t;
+
+/* #define TEST > */
+/* typedef unsigned char data_t; */
+
+/* #define TEST != */
+/* typedef int data_t; */
 
 int test(data_t a) {
     return a TEST 0;
