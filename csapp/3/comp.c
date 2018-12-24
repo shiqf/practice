ef2841774f296d12e8d8d56117b2cc19a9dab5fa
shiqf <jiyuuinchina@hotmail.com>
2018-12-24 21:28:07 +0800

    update

--- csapp/3/comp.c
+++ csapp/3/comp.c
@@ -3,8 +3,13 @@
 /* cmp    %esi,%edi */
 /* setl   %al */
 
+/* #define COMP <= */
+/* typedef unsigned short data_t; */
+/* cmp    %si,%di */
+/* setbe  %al */
+
 #define COMP <=
-typedef unsigned short data_t;
+typedef unsigned char data_t;
 
 int comp(data_t a, data_t b) {
     return a COMP b;
