6a08bca27b0f2a8ae4deb3581aa4f65ccd165d2d
shiqf <jiyuuinchina@hotmail.com>
2018-12-24 20:53:38 +0800

    result

--- csapp/3/3.4.c
+++ csapp/3/3.4.c
@@ -1,6 +1,32 @@
-
 typedef long src_t;
 typedef long dest_t;
+/* mov    (%rdi),%rax */
+/* mov    %rax,(%rsi) */
+
+/* typedef char src_t; */
+/* typedef short dest_t; */
+/* movsbw (%rdi),%ax */
+/* mov    %ax,(%rsi) */
+
+/* typedef char src_t; */
+/* typedef unsigned dest_t; */
+/* movsbl (%rdi),%eax */
+/* mov    %eax,(%rsi) */
+
+/* typedef unsigned char src_t; */
+/* typedef long dest_t; */
+/* movzbl (%rdi),%eax */
+/* mov    %rax,(%rsi) */
+
+/* typedef int src_t; */
+/* typedef char dest_t; */
+/* mov    (%rdi),%eax */
+/* mov    %al,(%rsi) */
+
+/* typedef unsigned src_t; */
+/* typedef unsigned char dest_t; */
+/* mov    (%rdi),%eax */
+/* mov    %al,(%rsi) */
 
 /* typedef char src_t; */
 /* typedef short dest_t; */
