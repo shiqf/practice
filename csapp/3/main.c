d7499c3627f78af3e8d2ce553b381bc38cc929e7
shiqf <jiyuuinchina@hotmail.com>
2018-12-25 20:13:26 +0800

    test

--- csapp/3/main.c
+++ csapp/3/main.c
@@ -7,6 +7,17 @@ int main(void)
     long d;
     multstore(2, 3, &d);
     printf("2 * 3 --> %ld\n", d);
+    
+    long sum = 0;
+    long i;
+    for (i = 0; i < 10; ++i) {
+        if (i & 1)
+            continue;
+        printf("i = %ld\n", i);
+        sum += i;
+    }
+    
+    printf("sum = %ld\n", sum);
 
     return 0;
 }
