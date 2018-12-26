e00df5d9cc3bff8e8d25a8477ea97b5a08b7050f
shiqf <jiyuuinchina@hotmail.com>
2018-12-26 15:47:13 +0800

    fix

--- csapp/3/switcher.c
+++ csapp/3/switcher.c
@@ -2,9 +2,24 @@ void switcher(long a, long b, long c, long *dest) {
     long val = 0;
 
     switch (a) {
-        case 6:
+        case 5:
+            c = 15 ^ b;
+
+        case 0:
+            val = c + 112;
             break;
-        /* default: */
+
+        case 2:
+        case 7:
+            val = (b + c) << 2;
+            break;
+
+        case 4:
+            val = a;
+            break;
+
+        default:
+            val = b;
     }
 
     *dest = val;
