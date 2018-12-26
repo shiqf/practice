7014ac4cc6856d6613b7d2e797189e912a1ca6ab
shiqf <jiyuuinchina@hotmail.com>
2018-12-26 09:32:22 +0800

    switcher.c

---
+++ csapp/3/switcher.c
@@ -0,0 +1,11 @@
+void switcher(long a, long b, long c, long *dest) {
+    long val = 0;
+
+    switch (a) {
+        case 6:
+            break;
+        /* default: */
+    }
+
+    *dest = val;
+}
