586c083ca324646f40c699a23da7474f8a1590e9
shiqf <jiyuuinchina@hotmail.com>
2018-12-30 19:39:14 +0800

    add sum_element.c

--- csapp/test/main.c
+++ csapp/test/main.c
@@ -2,7 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    printf("%ld\n", sizeof(long));
+    short S[7];
+    short *T[3];
+    short **U[6];
+    int V[8];
+    double *W[4];
+
+    printf("%ld %ld %ld %ld %ld\n", sizeof(S), sizeof(T), sizeof(U), sizeof(V), sizeof(W));
 
     return 0;
 }
