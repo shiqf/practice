715900fbe56d8cd1e37ae630493970e28c5a0cff
shiqf <jiyuuinchina@hotmail.com>
2019-03-17 13:29:45 +0000

    add exerices.cc

---
+++ cpp_primer/2/exercise.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/* /1* 2.10 *1/ */
+/* string global_str; */
+/* int global_int; */
+
+int main(void)
+{
+
+    /* unsigned u = 10, u2 = 42; */
+    /* cout << u2 - u << endl; */
+    /* cout << u - u2 << endl; */
+
+    /* int i = 10, i2 = 42; */
+    /* cout << i2 - i << endl; */
+    /* cout << i - i2 << endl; */
+    /* cout << i - u << endl; */
+    /* cout << u - i << endl; */
+
+    /* /1* 2.10 *1/ */
+    /* int local_int; */
+    /* string local_str; */
+    /* cout << "123" << global_int << " " << global_str << endl; */
+    /* cout << "456" << local_int << " " << local_str << endl; */
+
+    /* long double ld = 3.1415926536; */
+    /* int a{ld}, b = {ld}; */
+    /* int c{ld}, d = ld; */
+    /* int e = {3.14}; */
+    
+    return 0;
+}
