770e95498cd2e73df4669c6806c95f3a8f689b49
shiqf <jiyuuinchina@hotmail.com>
2019-03-17 20:55:27 +0800

    fix

--- test/file.c
+++ test/file.c
@@ -3,9 +3,12 @@
 
 int main(void)
 {
-    FILE *fp = fopen("./test.txt", "w");
+    char line[255];
+    FILE *fp = fopen("test.txt", "r");
+    fgets(line, 255, fp);
+    fgets(line, 255, fp);
+    printf("%s\n", line);
 
     fclose(fp);
-
     return 0;
 }
