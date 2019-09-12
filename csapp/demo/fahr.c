79aa6a6a1e5da82566a73d7a1c0eb555cf3ce8a2
shiqf <jiyuuinchina@hotmail.com>
2019-09-12 15:59:23 +0800

    fix

--- csapp/demo/fahr.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void fahr(float F)
-{
-    printf("%.2f\n", (5/9.0) * (F - 32));
-}
--- csapp/demo/test.h
+++
@@ -1,4 +0,0 @@
-#ifndef _TEST_H_
-#define _TEST_H_
-extern void fahr(float);
-#endif /* ifndef _TEST_H_ */
